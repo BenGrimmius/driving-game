715e1439d220772f169f9e7a8e9ca3cbdb12f994
Ben Grimmius <bengrimmius@gmail.com>
2022-12-16 00:17:40 +0000

    Complete issue-3-start-car

--- main.js
+++ main.js
@@ -11,3 +11,15 @@ document.addEventListener('keydown', function (event) {
     $car.className = 'east';
   }
 });
+
+document.addEventListener('keydown', function (event) {
+  if (event.keyCode === 32 && $car.className === 'east') {
+    $car.className = 'east go-east';
+  } else if (event.keyCode === 32 && $car.className === 'south') {
+    $car.className = 'south go-south';
+  } else if (event.keyCode === 32 && $car.className === 'west') {
+    $car.className = 'west go-west';
+  } else if (event.keyCode === 32 && $car.className === 'north') {
+    $car.className = 'north go-north';
+  }
+});
